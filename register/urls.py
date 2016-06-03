b5355ccfafabd483b77162bb88952402160c435b
kripaelizabethtom <kripa7elizabeth7tom1997@gmail.com>
2016-06-03 10:13:39 +0530

    django

---
+++ register/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from register import views
+from register.views import *
+from register.models import *
+from django.contrib.auth.decorators import login_required
+from register.views import UserRegistrationView
+
+
+urlpatterns = [
+url(r'^$', UserRegistrationView.as_view(), name='register_user'),
+    url(r'^user/$', UserRegistrationView.as_view(), name='register_user'),
+]
